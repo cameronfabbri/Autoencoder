9170456299f6b1274624bf42d5ab4cd99f6d3662
Cameron Fabbri <cameronfabbri@gmail.com>
2016-06-20 08:29:33 -0400

    eval almost complete

--- eval/eval.py
+++ eval/eval.py
@@ -16,16 +16,38 @@ import config
 result_file = config.result_file
 
 if __name__ == "__main__":
-   with open(result_file) as rf:
-      for line in rf:
-         line = line.rstrip().split("|")
-         print line
-         exit()
-         real_image = line[0]
-         gen_image  = line[1]
-        
-         print real_image
-         exit() 
+   with tf.Graph().as_default() as graph:
+
+      images = input_.inputs("test", batch_size, 1)
+
+      #sess = tf.Session()
+
+      variables_to_restore = tf.all_variables()
+
+      saver = tf.train.Saver(variables_to_restore)
+
+      summary_op = tf.werge_all_summaries()
+
+      summary_writer = tf.train.SummaryWriter(eval_dir, graph)
+
+      with tf.Session() as sess:
+
+         ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+         saver.restore(sess, ckpt.model_checkpoint_path)
+
+         global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+         coord = tf.train.Coordinator()
+
+         try:
+            tf.train.start_queue_runners(sess=sess)
+            threads = []
+
+            for q in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
+               threads.extend(q.create_threads(sess, coord=coord, daemon=True, start=True))
+
+               num_iter = int(math.ceil(num_examples/batch_size))
+
+               _, loss_value, generated_image, imgs = sess.run([train_op, loss, logits, images])
 
 
 
