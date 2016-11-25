2c42d57494bca6b328a8b0908f47de40a9aaaac1
Cameron Fabbri <cameronfabbri@gmail.com>
2016-11-24 22:29:41 -0500

    updates

--- autoencoder.py
+++ autoencoder.py
@@ -10,12 +10,12 @@ import os
 # import the functions we are going to use
 from tf_ops import conv2d, conv2d_transpose, fc_layer, lrelu
 
-batch_size = 10
+batch_size = 1000
 
 def encoder(x):
-   print
    # convolutional layer with a leaky Relu activation
    e_conv1 = lrelu(conv2d(x, 2, 2, 32, 'e_conv1'))
+   print
    print 'conv1: ', e_conv1
 
    # convolutional layer with a leaky Relu activation
@@ -37,22 +37,28 @@ def encoder(x):
    e_fc2 = lrelu(fc_layer(e_fc1, 256, False, 'e_fc2'))
    print 'fc2: ', e_fc2
 
-   return e_fc2
+   e_fc3 = lrelu(fc_layer(e_fc2, 128, False, 'e_fc3'))
+   print 'fc3: ', e_fc3
+   
+   return e_fc3
 
 def decoder(x):
    print
    print 'x: ', x
-  
-   # fully connected layer with a leaky Relu activation
-   d_fc1 = lrelu(fc_layer(x, 512, False, 'd_fc1'))
+ 
+   d_fc1 = lrelu(fc_layer(x, 256, False, 'd_fc2'))
+   print 'd_fc1: ', d_fc1
+
+   d_fc2 = lrelu(fc_layer(d_fc1, 512, False, 'd_fc3'))
+   print 'd_fc2: ', d_fc2
 
    # reshape for use in transpose convolution (deconvolution) 
    # must match conv layers in encoder
-   d_fc1 = tf.reshape(d_fc1, (batch_size, 4, 4, 32))
-   print 'd_fc1: ', d_fc1
+   d_fc2 = tf.reshape(d_fc2, (batch_size, 4, 4, 32))
+   print 'd_fc2: ', d_fc2
  
    # transpose convolution with a leaky relu activation
-   e_transpose_conv1 = lrelu(conv2d_transpose(d_fc1, 2, 2, 32, 'e_transpose_conv1'))
+   e_transpose_conv1 = lrelu(conv2d_transpose(d_fc2, 2, 2, 32, 'e_transpose_conv1'))
    print 'e_transpose_conv1: ', e_transpose_conv1
 
    # transpose convolution with a leaky relu activation
@@ -76,17 +82,18 @@ def train(mnist_train, mnist_test):
       # placeholder for mnist images
       images      = tf.placeholder(tf.float32, [batch_size, 28, 28, 1])
 
-      # encode images to 256 dim vector
+      # encode images to 128 dim vector
       encoded = encoder(images)
 
-      # decode 256 dim vector to (28,28) dim image
+      # decode 128 dim vector to (28,28) dim image
       decoded = decoder(encoded)
 
-      # loss is the squared error between the true image and the one we
-      # decoded from the 256 dim vector
-      loss = tf.reduce_mean(tf.pow(images - decoded, 2))
+      loss = tf.nn.l2_loss(images - decoded)
      
       train_op = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(loss)
+
+      # saver for the model
+      saver = tf.train.Saver(tf.all_variables())
 
       init = tf.initialize_all_variables()
       sess = tf.Session()
@@ -96,6 +103,19 @@ def train(mnist_train, mnist_test):
          os.mkdir('images/')
       except:
          pass
+      try:
+         os.mkdir('checkpoint/')
+      except:
+         pass
+
+      ckpt = tf.train.get_checkpoint_state('checkpoint/')
+      if ckpt and ckpt.model_checkpoint_path:
+         try:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            print 'Model restored'
+         except:
+            print 'Could not restore model'
+            pass
 
       step = 0
       while True:
@@ -108,10 +128,11 @@ def train(mnist_train, mnist_test):
          _, loss_ = sess.run([train_op, loss], feed_dict={images: batch_images})
          print 'Step: ' + str(step) + ' Loss: ' + str(loss_)
 
-         if step%1000 == 0:
+         if step%100 == 0:
             print
             print 'Saving model'
             print
+            saver.save(sess, "checkpoint/checkpoint", global_step=global_step)
 
             # get random images from the test set
             batch_images = random.sample(mnist_test, batch_size)
@@ -121,10 +142,11 @@ def train(mnist_train, mnist_test):
 
             # write out a few
             c = 0
-            for img in encode_decode:
-               img = np.squeeze(img)
-               plt.imsave('images/image_'+str(step)+'_'+str(c)+'.png', img)
-               if c == 10:
+            for real, dec in zip(batch_images, encode_decode):
+               dec, real = np.squeeze(dec), np.squeeze(real)
+               plt.imsave('images/'+str(step)+'_'+str(c)+'real.png', real)
+               plt.imsave('images/'+str(step)+'_'+str(c)+'dec.png', dec)
+               if c == 5:
                   break
                c+=1
 
