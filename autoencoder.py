3b2ef7cffe755ea88712169a4528d54503cf8d91
Cameron Fabbri <cameronfabbri@gmail.com>
2016-11-24 20:20:06 -0500

    updates functions

--- autoencoder.py
+++ autoencoder.py
@@ -3,73 +3,65 @@ import cPickle as pickle
 import tensorflow as tf
 import numpy as np
 import requests
+import random
 import gzip
 import os
 
 # import the functions we are going to use
-from tensorflow_ops import _conv_layer, _transpose_conv_layer, _fc_layer, lrelu
+from tf_ops import conv2d, conv2d_transpose, fc_layer, lrelu
 
 batch_size = 10
 
 def encoder(x):
-   # convolutional layer with a leaky Relu activation
-   e_conv1 = lrelu(_conv_layer(x, 2, 2, 32, 'e_conv1'))
-   print 'conv1: ', e_conv1
    print
+   # convolutional layer with a leaky Relu activation
+   e_conv1 = lrelu(conv2d(x, 2, 2, 32, 'e_conv1'))
+   print 'conv1: ', e_conv1
 
    # convolutional layer with a leaky Relu activation
-   e_conv2 = lrelu(_conv_layer(e_conv1, 2, 2, 64, 'e_conv2'))
+   e_conv2 = lrelu(conv2d(e_conv1, 2, 2, 64, 'e_conv2'))
    print 'conv2: ', e_conv2
-   print
    
    # convolutional layer with a leaky Relu activation
-   e_conv3 = lrelu(_conv_layer(e_conv2, 2, 2, 32, 'e_conv3'))
+   e_conv3 = lrelu(conv2d(e_conv2, 2, 2, 32, 'e_conv3'))
    print 'conv3: ', e_conv3
-   print
   
    # fully connected layer with a leaky Relu activation
    # The 'True' here means that we are flattening the input
-   e_fc1 = lrelu(_fc_layer(e_conv2, 512, True, 'e_fc1'))
+   e_fc1 = lrelu(fc_layer(e_conv2, 512, True, 'e_fc1'))
    print 'fc1: ', e_fc1
-   print
 
    # fully connected layer with a leaky Relu activation
    # the output from the previous fully connected layer is
    # already flat, so no need to flatten, hence 'False'
-   e_fc2 = lrelu(_fc_layer(e_fc1, 256, False, 'e_fc2'))
+   e_fc2 = lrelu(fc_layer(e_fc1, 256, False, 'e_fc2'))
    print 'fc2: ', e_fc2
-   print
 
    return e_fc2
 
 def decoder(x):
-   print 'x: ', x
    print
+   print 'x: ', x
   
-   
    # fully connected layer with a leaky Relu activation
-   d_fc1 = lrelu(_fc_layer(x, 512, False, 'd_fc1'))
+   d_fc1 = lrelu(fc_layer(x, 512, False, 'd_fc1'))
 
    # reshape for use in transpose convolution (deconvolution) 
    # must match conv layers in encoder
    d_fc1 = tf.reshape(d_fc1, (batch_size, 4, 4, 32))
    print 'd_fc1: ', d_fc1
-   print
  
    # transpose convolution with a leaky relu activation
-   e_transpose_conv1 = lrelu(_transpose_conv_layer(d_fc1, 2, 2, 32, 'e_transpose_conv1'))
+   e_transpose_conv1 = lrelu(conv2d_transpose(d_fc1, 2, 2, 32, 'e_transpose_conv1'))
    print 'e_transpose_conv1: ', e_transpose_conv1
-   print
 
    # transpose convolution with a leaky relu activation
-   e_transpose_conv2 = lrelu(_transpose_conv_layer(e_transpose_conv1, 2, 2, 64, 'e_transpose_conv2'))
+   e_transpose_conv2 = lrelu(conv2d_transpose(e_transpose_conv1, 2, 2, 64, 'e_transpose_conv2'))
    print 'e_transpose_conv2: ', e_transpose_conv2
-   print
    
    # transpose convolution with a leaky relu activation
-   e_transpose_conv3 = lrelu(_transpose_conv_layer(e_transpose_conv2, 2, 2, 1, 'e_transpose_conv3'))
+   e_transpose_conv3 = lrelu(conv2d_transpose(e_transpose_conv2, 2, 2, 1, 'e_transpose_conv3'))
    print 'e_transpose_conv3: ', e_transpose_conv3
-   print
 
    # since transpose convs make the resolution go 4->8->16->32 (because stride 2)
    # we need to crop to original mnist size (28,28)
@@ -100,6 +92,11 @@ def train(mnist_train, mnist_test):
       sess = tf.Session()
       sess.run(init)
 
+      try:
+         os.mkdir('images/')
+      except:
+         pass
+
       step = 0
       while True:
          step += 1
@@ -112,6 +109,9 @@ def train(mnist_train, mnist_test):
          print 'Step: ' + str(step) + ' Loss: ' + str(loss_)
 
          if step%1000 == 0:
+            print
+            print 'Saving model'
+            print
 
             # get random images from the test set
             batch_images = random.sample(mnist_test, batch_size)
@@ -119,11 +119,14 @@ def train(mnist_train, mnist_test):
             # encode them using the encoder, then decode them
             encode_decode = sess.run(decoded, feed_dict={images: batch_images})
 
-            # write out to files
+            # write out a few
+            c = 0
             for img in encode_decode:
                img = np.squeeze(img)
-               plt.imsave('images/image_'+str(step)+'.png', img)
-
+               plt.imsave('images/image_'+str(step)+'_'+str(c)+'.png', img)
+               if c == 10:
+                  break
+               c+=1
 
 def main(argv=None):
    # mnist data in gz format
